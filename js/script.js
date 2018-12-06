0249775c486f86112c91794ec2ec1a04243d6ea7
Joe Wheelock <joemwheelock@gmail.com>
2018-12-05 20:59:29 -0800

    createList function added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
 //variables/queries
 const btn = document.querySelector('.btn');
-const TodoArr = [];
+const todoUl = document.querySelector('.todo-ul');
+const todoArr = [];
 
 // User submits item to Todo Array
 btn.addEventListener('click', e => {
   e.preventDefault();
 
   let item = document.forms.todoForm.todoItem;
-  console.log(item.value);
-  item.value= '';
+  if (item.value.length > 0) {
+    todoArr.push(item.value);
+    console.log(todoArr);
+    createList();
+    item.value= '';
+  };
 });
+
+// Create List from Array
+function createList() {
+  todoUl.innerHTML = ''; //clears out existing list
+  for (let task of todoArr) {
+    const todoLi = document.createElement('li');
+    todoLi.textContent = task;
+    todoUl.appendChild(todoLi);
+  }
+};
