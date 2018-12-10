1a5dd02647c0c5a8a4f92e02ac426cca19ae2539
Joe Wheelock <joemwheelock@gmail.com>
2018-12-10 04:44:59 -0800

    glyphicons and some css changes

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 const btn = document.querySelector('.btn');
 const todoUl = document.querySelector('.todo-ul');
 const todoArr = [];
+const donezoArr = [];
 
 // User submits item to Todo Array
 btn.addEventListener('click', e => {
@@ -12,16 +13,28 @@ btn.addEventListener('click', e => {
     todoArr.push(item.value);
     console.log(todoArr);
     createList();
-    item.value= '';
+    item.value= ''; //clears entry form
   };
 });
 
-// Create List from Array
+// Create List Items from Array
 function createList() {
   todoUl.innerHTML = ''; //clears out existing list
   for (let task of todoArr) {
     const todoLi = document.createElement('li');
+    todoLi.classList.add('col-10', 'offset-1', 'list-item');
+
+    const checkBox = document.createElement('i');
+    checkBox.classList.add('far', 'fa-square');
+
+    const editBox = document.createElement('i')
+    editBox.classList.add('fas', 'fa-edit');
+
+    const deleteBox = document.createElement('i');
+    deleteBox.classList.add('fas', 'fa-minus-square');
+
     todoLi.textContent = task;
+    todoLi.append(checkBox, editBox, deleteBox);
     todoUl.appendChild(todoLi);
   }
 };
